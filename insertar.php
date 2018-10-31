5d00abfc4f905a3271e755cca7e77db1a6c746c7
Jose Maria Gallego <josema.m.g.95@hotmail.com>
2018-10-31 20:37:22 +0100

    Pongo los id que faltaban en los inputs para acompañar al label

--- insertar.php
+++ insertar.php
@@ -8,15 +8,15 @@
         <h3>Insertar una nueva pelicula</h3>
         <form action="" method="post">
             <label for="titulo">Titulo</label>
-            <input type="text" name="titulo" ><br />
+            <input type="text" name="titulo" id="titulo" ><br />
             <label for="anyo">Año</label>
-            <input type="text" name="anyo" ><br />
+            <input type="text" name="anyo" id="anyo"><br />
             <label for="sinopsis">Sinopsis</label>
-            <textarea name="sinopsis" rows="8" cols="80"></textarea><br />
+            <textarea name="sinopsis" rows="8" cols="80" id="sinopsis"></textarea><br />
             <label for="duracion">Duración</label>
-            <input type="text" name="duracion" ><br />
+            <input type="text" name="duracion" id="duracion"><br />
             <label for="genero_id">Género</label>
-            <input type="text" name="genero_id" ><br />
+            <input type="text" name="genero_id" id="genero_id"><br />
             <input type="submit" value="Insertar">
             <a href="index.php">Volver</a>
         </form>
