135df12cdbe77276651c9b04b2d26ca123ec75d0
Jose Maria Gallego <josemaria.gallego@iesdonana.org>
2018-11-13 12:00:44 +0100

    Coloco el pie de página en todos los modulo

--- generos/confirm_borrado.php
+++ generos/confirm_borrado.php
@@ -21,7 +21,7 @@
            $_SESSION['mensaje'] = 'Debe ser administrador para poder borrar películas';
            irAlIndice();
        }
-       
+
         if (isset($_GET['id'])) {
             $id = $_GET['id'];
         } else {
@@ -45,6 +45,7 @@
             </div>
           </div>
         </div>
+        <?php piePagina(); ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
